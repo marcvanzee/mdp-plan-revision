653143ccd46d7826ed8c8a320f5ee54cc8073ba7
Marc van Zee <marcvanzee@gmail.com>
2015-10-15 13:42:58 +0200

    Started implementing optimized tileworld for hypothetical simulations.

--- src/simulations/TileworldSimulation.java
+++ src/simulations/TileworldSimulation.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 import constants.MathOperations;
-import constants.Printing;
 import mdp.Tileworld;
 import mdp.agent.Agent;
 import mdp.agent.Angel;
